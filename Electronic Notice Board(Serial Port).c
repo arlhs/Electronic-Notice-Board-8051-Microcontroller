af95855cc8342986ff0ad924f5f985fbf2b38512
Aditya Raj <adityaraj.lhs007@gmail.com>
2020-11-27 01:18:44 +0530

    Comment Added

--- Electronic Notice Board(Serial Port).c
+++ Electronic Notice Board(Serial Port).c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<at89c51xd2.h>
 
-void lcd_comm(void); 
+void lcd_comm(void);
 void delay(int);
 void lcd_data(unsigned char);
 void lcd_init(void);
@@ -11,12 +11,14 @@ unsigned char temp2;
 unsigned char var,j;
 sbit RS = P2^7;
 sbit EN = P2^6;
-sbit RW = P2^5;		   
+sbit RW = P2^5;
 unsigned char x;
 unsigned char i=0;
-   
+
+#lets see what status of git shows to me
+
 int main()
-{	
+{
 lcd_init();
 
 SCON = 0x50;
@@ -38,7 +40,7 @@ while(1)
 	lcd_init2();
 	}
 }
-  
+
 void lcd_init(void)
 {
 unsigned int cmd[]={0x20,0x28,0X06,0X0E,0X0E,0X80};
@@ -63,43 +65,43 @@ for(j=0;j<6;j++)
 	}
  }
 
-void lcd_comm(void) 
+void lcd_comm(void)
 {
-	var = temp1; 
-  	temp1 = temp1 & 0xf0; 
-  	temp1 = temp1 >> 4;		
+	var = temp1;
+  	temp1 = temp1 & 0xf0;
+  	temp1 = temp1 >> 4;
 	P2 = temp1;
-    RS=0;							   
-    EN=1;   
+    RS=0;
+    EN=1;
 	delay(10);
-    EN=0;	
-  	temp1 = var & 0x0f;		
+    EN=0;
+  	temp1 = var & 0x0f;
 	P2 = temp1;
-    RS=0;							 
-    EN=1;   
+    RS=0;
+    EN=1;
 	delay(10);
     EN=0;
 }
 
 void lcd_data(unsigned char temp2)
 {
-   	var = temp2; 
-  	temp2 = temp2 & 0xf0;  
-  	temp2 = temp2 >> 4;   
+   	var = temp2;
+  	temp2 = temp2 & 0xf0;
+  	temp2 = temp2 >> 4;
   	P2 = temp2;
-	RS=1;		 
+	RS=1;
 	EN=1;
 	delay(10);
 	EN=0;
-  	temp2 = var & 0x0f;	   
+  	temp2 = var & 0x0f;
 	P2 = temp2;
-	RS=1;		 
+	RS=1;
 	EN=1;
 	delay(10);
 	EN=0;
    	delay(5000);
 	i++;
-}  
+}
 
 void delay(int count)
 {
